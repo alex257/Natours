810e6815df19ae0206526ab2bcbe5cc562f8b6d5
AlxD11 <damnjanovic_aleksandra@yahoo.com>
2019-10-06 18:03:59 +0200

    fix gulp outdated issues

--- gulpfile.js
+++ gulpfile.js
@@ -1,70 +1,34 @@
-const gulp         = require('gulp'),
-      sass         = require('gulp-sass'),
-      browserSync  = require('browser-sync').create(),
-      autoprefixer =require('gulp-autoprefixer');
+const gulp = require("gulp");
+const sass = require("gulp-sass");
+const sourcemaps = require("gulp-sourcemaps");
+const autoprefixer = require("gulp-autoprefixer");
+const browserSync = require("browser-sync").create();
 
-	 
-      
-	 function style () {
-	 
-        return gulp.src('src/scss/**/*.scss',{
-              sourcemaps: true
-            })
-        
-         .pipe(sass().on('error', sass.logError))
+const sassOptions = {
+  outputStyle: "expanded"
+};
 
-         .pipe(autoprefixer({
-            browsers: ['last 2 version', '> 5%'],
-            cascade: false
-          }))
-       
-         .pipe(gulp.dest("src/css"))
-          
-         .pipe(browserSync.stream())
-       
-       };
-  
-       /*function copyHtml () {
-       
-          return  gulp.src('src/*.html')
-         
-           .pipe(gulp.dest('dist'))
-            
-           //.pipe(browserSync.stream());
-         
-         };
-  
-      function copyImages () {
-       
-          return  gulp.src('src/img/*.{gif,jpg,png,svg}')
-           .pipe(gulp.dest('dist/img'))
-         }; 
-       */
-  
-       function watch(){
-          
-        browserSync.init({
-            server: "./src"
-        });
-    
-      
-          gulp.watch('src/scss/**/*.scss', style); 
-          //gulp.watch('src/*.html', copyHtml);
-          gulp.watch('src/*.html').on('change', browserSync.reload);
-          //gulp.watch('src/img/*.{gif,jpg,png,svg}', copyImages);
-          //gulp.watch('src/js/*.js').on('change', browserSync.reload);  
-       }
-       
-       
-       exports.style = style;
-       //exports.copyHtml = copyHtml;
-       //exports.copyImages = copyImages;
-       exports.watch = watch;
-      // exports.default = build;
-       
-       
-      
-      var build = gulp.parallel(style, watch);
-      gulp.task(build);
-      gulp.task('default', build);
-      
+// compile scss into css
+
+function style() {
+  return gulp
+    .src("./scss/**/*.scss")
+    .pipe(sourcemaps.init())
+    .pipe(sass(sassOptions).on("error", sass.logError))
+    .pipe(sourcemaps.write())
+    .pipe(autoprefixer())
+    .pipe(gulp.dest("./css"))
+    .pipe(browserSync.stream());
+}
+
+function watch() {
+  browserSync.init({
+    server: "."
+  });
+  gulp.watch("./scss/**/*.scss", style);
+  gulp.watch("./*.html").on("change", browserSync.reload);
+  //gulp.watch("./js/**/*.js", browserSync.reload);
+}
+
+exports.style = style;
+exports.watch = watch;
